a782aed8fdb6b6fce5ba313bf8264ca553b136b6
rkjellberg <rkjellberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-01 23:30:52 +0000

    Added missed files
    git-svn-id: https://plugins.svn.wordpress.org/umbrella-antivirus-hack-protection/tags/1.1@1102859 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ src/modules/captcha_login.php
@@ -0,0 +1,36 @@
+<?php
+// Displaying the Captcha Field in the Login Form
+add_action('login_form', function (){
+  if(class_exists('ReallySimpleCaptcha'))
+  {
+    $captcha_instance = new ReallySimpleCaptcha();
+    $word = $captcha_instance->generate_random_word();
+    $prefix = mt_rand();
+    $captchaimg = $captcha_instance->generate_image( $prefix, $word );
+    $imgpath = plugins_url().'/really-simple-captcha/tmp/'.$captchaimg;
+
+    echo "<input type='hidden' name='umbrella_captcha_prefix' value='{$prefix}'/>";
+    echo "<img src='{$imgpath}' style='float: right;position: absolute;margin-left: 190px;margin-top: 11px;' /><input name='umbrella_captcha_text' type='text' />";
+  }
+});
+
+// Validating the Captcha
+add_filter('wp_authenticate_user', function($user, $password) {
+	$return_value = $user;
+
+	if(class_exists('ReallySimpleCaptcha')){
+
+		$captcha_instance = new ReallySimpleCaptcha();
+		$prefix = $_POST['umbrella_captcha_prefix'];
+		if(!$captcha_instance->check( $prefix, $_POST['umbrella_captcha_text'] ))
+		{
+		  // if there is a mis-match
+		  $return_value = new WP_Error( 'loginCaptchaError', 'Captcha Error. Please try again.' );
+		}
+
+		// remember to remove the prefix
+		$captcha_instance->remove( $prefix );
+	}
+	return $return_value;
+
+} ,10,2);
